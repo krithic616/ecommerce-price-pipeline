f7f6680f20518fe061e9835d0d27dc686400fb3c
krithic616 <krithic484@gmail.com>
2026-02-11 22:06:23 +0530

    Fix indentation error

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -2,27 +2,27 @@ import requests
 import pandas as pd
 from datetime import datetime
 
-# API endpoint
-URL = "https://fakestoreapi.com/products"
-
 def fetch_data():
-    response = requests.get(URL)
+    url = "https://fakestoreapi.com/products"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
         raise Exception(f"API request failed with status {response.status_code}")
 
     data = response.json()
-    df = pd.DataFrame(data)
 
-    # Add extraction timestamp
+    df = pd.DataFrame(data)
     df["extracted_at"] = datetime.now()
 
     return df
 
-def save_raw_data(df):
-    df.to_csv("data/raw_data.csv", index=False)
-    print("Raw data saved successfully.")
 
 if __name__ == "__main__":
     df = fetch_data()
-    save_raw_data(df)
+    df.to_csv("data/raw_data.csv", index=False)
+    print("Raw data saved successfully.")
