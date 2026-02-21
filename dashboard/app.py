663bdb0a4ea587063f218db9952125195ed0be88
Krithic Babu <140595486+krithic616@users.noreply.github.com>
2026-02-21 18:25:19 +0530

    Refactor data loading and update last updated timestamp

--- dashboard/app.py
+++ dashboard/app.py
@@ -6,10 +6,10 @@ st.set_page_config(page_title="E-Commerce Price Dashboard", layout="wide")
 
 st.title("📊 E-Commerce Price Monitoring Dashboard")
 
-# GitHub raw CSV URL
+
 DATA_URL = "https://raw.githubusercontent.com/krithic616/ecommerce-price-pipeline/main/data/cleaned_data.csv"
 
-# Load data
+
 @st.cache_data
 def load_data():
     df = pd.read_csv(DATA_URL)
@@ -17,17 +17,17 @@ def load_data():
 
 df = load_data()
 
-# Convert extracted_at column to datetime
+
 df["extracted_at"] = pd.to_datetime(df["extracted_at"])
 
-# Last updated timestamp
+
 last_updated = df["extracted_at"].max()
 
 st.markdown(f"### 🕒 Last Updated: {last_updated.strftime('%d %b %Y - %I:%M %p')}")
 
 st.divider()
 
-# Metrics
+
 col1, col2, col3 = st.columns(3)
 
 col1.metric("Total Products", len(df))
